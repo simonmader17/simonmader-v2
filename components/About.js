c0fb28a1d9ce91d59f43153f1e707cbac395b34f
simonmader17 <seticher.mader@gmail.com>
2022-02-12 13:45:32 +0100

    Changed the content of the about component

--- components/About.js
+++ components/About.js
@@ -1,4 +1,12 @@
+import moment from "moment";
+
 function About() {
+  const calculateAge = () => {
+    const birthday = moment(new Date("2002-02-17"));
+    const today = moment();
+    return today.diff(birthday, 'years');
+  }
+
   return (
     <div>
       <h2 id="about" className="m-4 pt-4 text-2xl font-bold">
@@ -10,12 +18,16 @@ function About() {
             Hi! Ich bin Simon.
           </p>
           <p className={"md:text-justify md:text-xl"}>
-            Ich habe mit dem Schuljahr 2020/21 die HTL St. Pölten (Informatik)
-            mit ausgezeichnetem Erfolg abgeschlossen. Der Fokus in der Schule
-            lag auf Java Spring Development, Algorithmik und Datenbankdesign.
-            Privat beschäftige ich mich gerne mit der Automatisierung von
-            Vorgängen mithilfe von Python und der Entwicklung von Webapps
-            mithilfe von JavaScript Frameworks wie React.
+            Ich bin {calculateAge()} Jahre alt und habe mit dem Schuljahr 2020/21 die HTL St.
+            Pölten (Informatik) mit ausgezeichnetem Erfolg abgeschlossen. Der
+            Fokus in der Schule lag auf Java Spring Development, Algorithmik und
+            Datenbankdesign. Privat beschäftige ich mich gerne mit der
+            Automatisierung von Vorgängen mithilfe von Python und der
+            Entwicklung von Webapps mithilfe von JavaScript Frameworks wie
+            React.
+          </p>
+          <p className="md:text-justify md:text-xl">
+              Zurzeit leiste ich meinen Zivildienst beim Samariterbund in St. Pölten.
           </p>
           <p className={"md:text-justify md:text-xl"}>
             In meiner Freizeit mache ich gerne Kraftsport, beschäftige ich mich
