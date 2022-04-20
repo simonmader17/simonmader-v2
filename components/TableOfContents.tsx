37dbe0e4011d0e59844cb8aa3d75e0829c384d68
simonm <seticher.mader@gmail.com>
2022-04-20 19:17:28 +0200

    Added i18n to the toc

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,10 +1,14 @@
+import useTranslation from "next-translate/useTranslation";
+
 const TableOfContents = ({ outline, pages, onItemClick, ...props }) => {
+  const { t } = useTranslation("diploma-thesis");
+
   outline = outline.filter(({ pageNumber }) => pages.includes(pageNumber));
 
   return (
     <div {...props}>
       <p className="select-text pb-2 text-lg text-red-400 md:text-xl">
-        Inhaltsverzeichnis
+        {t("toc")}
       </p>
       {outline.map((pair) => (
         <p
