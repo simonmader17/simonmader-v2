adab7fbc5f481b7afe0149d1f0b1d3629520e214
simonm <seticher.mader@gmail.com>
2022-04-22 20:02:12 +0200

    Optimized ripple effect

--- lib/ripple.ts
+++ lib/ripple.ts
@@ -17,24 +17,16 @@ const createRipple = (event: React.MouseEvent<Element, MouseEvent>) => {
   }px`;
   ripple.classList.add("ripple");
 
-  // Check for existing ripples
-  const prevRipple = button.getElementsByClassName("ripple")[0];
-  if (prevRipple) {
-    prevRipple.remove();
-  }
-
   // Add ripple
   button.insertBefore(ripple, button.firstChild);
 
   // Add listeners to make the ripple effect fade out
-  button.addEventListener("pointerup", fadeOutRipple);
-  button.addEventListener("pointercancel", fadeOutRipple);
-  button.addEventListener("pointerleave", fadeOutRipple);
+  button.addEventListener("pointerup", () => fadeOutRipple(ripple));
+  button.addEventListener("pointercancel", () => fadeOutRipple(ripple));
+  button.addEventListener("pointerleave", () => fadeOutRipple(ripple));
 };
 
-const fadeOutRipple = (event: MouseEvent) => {
-  const button = event.currentTarget as HTMLElement;
-  const ripple = button.getElementsByClassName("ripple")[0] as HTMLSpanElement;
+const fadeOutRipple = (ripple: HTMLSpanElement) => {
   if (ripple) ripple.classList.add("ripple-fade-out");
   const removeRipple = async () => {
     await new Promise((res) => setTimeout(res, 300));
