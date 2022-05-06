3933ef5cde7362d047d630e437bba7ff85e3e9f4
simonm <seticher.mader@gmail.com>
2022-05-06 22:05:06 +0200

    Fixed pixelated down chevron

--- components/PixelatedDownChevron.tsx
+++ components/PixelatedDownChevron.tsx
@@ -6,10 +6,7 @@ const PixelatedDownChevron = ({
   style?: Object;
 }) => {
   return (
-    <div
-      className={["h-[calc(3rem*4/7)] w-12", className].join(" ")}
-      style={style}
-    >
+    <div className={`h-[calc(3rem*4/7)] w-12 ${className}`} style={style}>
       <span className="absolute left-0 top-0 inline-block h-1/4 w-[calc(100%/7)] bg-red-400" />
       <span className="absolute left-[calc(100%/7)] top-1/4 inline-block h-1/4 w-[calc(100%/7)] bg-red-400" />
       <span className="absolute left-[calc(2*100%/7)] top-1/2 inline-block h-1/4 w-[calc(100%/7)] bg-red-400" />
