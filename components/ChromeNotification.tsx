087abc0f63d1298826bf41fb5d2a4b878f858cbe
simonm <seticher.mader@gmail.com>
2022-05-12 18:10:38 +0200

    Small adjustments

--- components/ChromeNotification.tsx
+++ components/ChromeNotification.tsx
@@ -15,39 +15,38 @@ function ChromeNotification() {
   return (
     <>
       {isFirefox && (
-        <div
-          ref={ref}
-          className="drop-shadow-3xl fixed top-4 right-4 z-50 flex w-80 items-center rounded-xl bg-black bg-opacity-50 p-2"
-        >
-          <div className="relative m-4 h-12 w-12 flex-shrink-0">
-            <Image
-              src={chrome_icon}
-              alt="Chrome Icon"
-              layout="fill"
-              objectFit="cover"
-              objectPosition="center"
-            />
-          </div>
-          <p className="text-left">For a better experience use Chrome :)</p>
-          <button
-            className="self-start rounded-full p-2 transition-colors hover:bg-white hover:bg-opacity-10"
-            onClick={() => (ref.current as HTMLDivElement).remove()}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
+        <div ref={ref} className="md:drop-shadow-3xl fixed top-4 right-4 z-50">
+          <div className="clip-rounded-pixel flex w-80 items-center rounded-xl bg-black bg-opacity-50 p-2">
+            <div className="relative m-4 h-12 w-12 flex-shrink-0">
+              <Image
+                src={chrome_icon}
+                alt="Chrome Icon"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
               />
-            </svg>
-          </button>
+            </div>
+            <p className="text-left">For a better experience use Chrome :)</p>
+            <button
+              className="self-start rounded-full p-2 transition-colors hover:bg-white hover:bg-opacity-10"
+              onClick={() => (ref.current as HTMLDivElement).remove()}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
       )}
     </>
