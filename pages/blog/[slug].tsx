566cf2bce8c9458a19f1201911f28a44bcd76eea
simonm <seticher.mader@gmail.com>
2022-05-28 01:27:41 +0200

    Added thumbnail to the blog post and updated sitemap

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -6,6 +6,8 @@ import { serialize } from "next-mdx-remote/serialize";
 import Head from "next/head";
 import Container from "../../components/Container";
 import rehypeHighlight from "rehype-highlight";
+import { getPlaiceholder } from "plaiceholder";
+import Image from "next/image";
 
 export async function getStaticPaths() {
   const posts = fs.readdirSync(
@@ -31,32 +33,54 @@ export async function getStaticProps({ params }) {
   );
   const { content, data } = matter(postSource);
 
-  console.log(data);
+  const thumbnailPath = "/images/blog/posts/" + slug + "/" + data.thumbnail;
+  const thumbnailBlurDataURL = await (
+    await getPlaiceholder(thumbnailPath)
+  ).base64;
 
   const mdxSource = await serialize(content, {
     mdxOptions: {
-      rehypePlugins: [rehypeHighlight]
-    }
+      rehypePlugins: [rehypeHighlight],
+    },
   });
 
-  return { props: { post: { source: mdxSource, data } } };
+  return {
+    props: {
+      post: { source: mdxSource, data, thumbnailPath, thumbnailBlurDataURL },
+    },
+  };
 }
 
 const Post = ({ post }) => {
-  const { source, data } = post;
+  const { source, data, thumbnailPath, thumbnailBlurDataURL } = post;
 
   return (
     <>
       <Head>
         <title>{data.title}</title>
         <meta name="description" content={data.description} />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css"
+        />
       </Head>
 
       <Container className="ltmd:mt-20">
         Published on: {data.publishedOn}
-        <h1 className="my-5 text-center md:my-10">{data.title}</h1>
-        <p>{data.description}</p>
+        <h1 className="mt-5 text-center md:mt-10">{data.title}</h1>
+        <div className="drop-shadow-pixel relative mx-auto mb-5 aspect-video w-full py-4 md:mb-10 md:w-2/3">
+          <Image
+            src={thumbnailPath}
+            alt={data.title}
+            layout="fill"
+            placeholder="blur"
+            blurDataURL={thumbnailBlurDataURL}
+            className="clip-rounded-pixel"
+          />
+        </div>
+        <p className="mb-5 text-center text-gray-400 md:mb-10">
+          {data.description}
+        </p>
         <MDXRemote {...source} />
       </Container>
     </>
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,30 +5,34 @@ import Head from "next/head";
 import Container from "../../components/Container";
 import { useRouter } from "next/router";
 import { createRipple } from "../../lib/ripple";
+import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
 
 export async function getStaticProps() {
   const postsFiles = fs.readdirSync(
     path.join(process.cwd(), "/components/pages/blog/posts")
   );
 
-  const posts = postsFiles
-    .map((post) => {
-      const source = fs.readFileSync(
-        path.join(process.cwd(), "/components/pages/blog/posts", post)
-      );
+  const promises = postsFiles.map(async (post) => {
+    const source = fs.readFileSync(
+      path.join(process.cwd(), "/components/pages/blog/posts", post)
+    );
 
-      const { data } = matter(source);
+    const { data } = matter(source);
 
-      const slug = post.replace(".mdx", "");
+    const slug = post.replace(".mdx", "");
 
-      return { data, slug };
-    })
-    .sort((a, b) => {
-      if (a.data.publishedOn < b.data.publishedOn) return 1;
-      return -1;
-    });
+    const thumbnailPath = "/images/blog/posts/" + slug + "/" + data.thumbnail;
+    const thumbnailBlurDataURL = await (
+      await getPlaiceholder(thumbnailPath)
+    ).base64;
 
-  console.log(posts);
+    return { data, slug, thumbnailPath, thumbnailBlurDataURL };
+  });
+  const posts = (await Promise.all(promises)).sort((a, b) => {
+    if (a.data.publishedOn < b.data.publishedOn) return 1;
+    return -1;
+  });
 
   return { props: { posts } };
 }
@@ -46,18 +50,30 @@ const Blog = ({ posts }) => {
       <Container className="ltmd:mt-20">
         <h1>Simon Mader&apos;s Blog ✍️</h1>
 
-        <p>Please do not expect regular content here...</p>
+        <p>Don&apos;t expect regular content here...</p>
 
         {posts.map((post) => (
-          <div
-            key={post.slug}
-            onClick={() => router.push(router.pathname + "/" + post.slug)}
-            className="clip-rounded-pixel relative m-4 cursor-pointer select-none bg-black bg-opacity-50 p-6"
-            onPointerDown={(e) => createRipple(e)}
-          >
-            <p className="text-sm text-gray-400">{post.data.publishedOn}</p>
-            <p className="text-red-400 md:text-lg">{post.data.title}</p>
-            <p className="text-sm">{post.data.description}</p>
+          <div key={post.slug} className="drop-shadow-pixel">
+            <div
+              onClick={() => router.push(router.pathname + "/" + post.slug)}
+              className="clip-rounded-pixel relative m-4 cursor-pointer select-none bg-black bg-opacity-50 md:flex md:justify-between"
+              onPointerDown={(e) => createRipple(e)}
+            >
+              <div className="p-6">
+                <p className="text-sm text-gray-400">{post.data.publishedOn}</p>
+                <p className="text-red-400 md:text-lg">{post.data.title}</p>
+                <p className="text-sm">{post.data.description}</p>
+              </div>
+              <div className="clip-rounded-pixel ltmd:w-full relative -z-10 aspect-video md:h-48">
+                <Image
+                  src={post.thumbnailPath}
+                  alt={post.title}
+                  layout="fill"
+                  placeholder="blur"
+                  blurDataURL={post.thumbnailBlurDataURL}
+                />
+              </div>
+            </div>
           </div>
         ))}
       </Container>
