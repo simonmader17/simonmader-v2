8357b396bec2c80bef0ade3b057808c8a6681156
simonm <seticher.mader@gmail.com>
2022-06-06 16:53:10 +0200

    Added animation to the navbar sub chapter indicator

--- components/pages/_app/Navbar/NavbarItemWithSubRefs.tsx
+++ components/pages/_app/Navbar/NavbarItemWithSubRefs.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 const variants = {
@@ -15,38 +15,53 @@ const NavbarItemWithSubRefs = ({ title, subRefs }) => {
         className="flex h-full cursor-pointer items-center justify-center px-5 hover:bg-white hover:bg-opacity-20"
         onClick={() => setOpen((open) => !open)}
       >
-        {subRefs &&
-          (open ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-1 h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19 9l-7 7-7-7"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-1 h-4 w-4"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
-          ))}
+        {subRefs && (
+          <div className="relative mr-1 flex h-4 w-4 items-center justify-center">
+            <AnimatePresence>
+              {open ? (
+                <motion.svg
+                  key="down"
+                  initial={{ opacity: 0, rotate: -90 }}
+                  animate={{ opacity: 1, rotate: 0 }}
+                  exit={{ opacity: 0, rotate: -90 }}
+                  style={{ originX: "50%", originY: "50%" }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19 9l-7 7-7-7"
+                  />
+                </motion.svg>
+              ) : (
+                <motion.svg
+                  key="right"
+                  initial={{ opacity: 0, rotate: 90 }}
+                  animate={{ opacity: 1, rotate: 0 }}
+                  exit={{ opacity: 0, rotate: 90 }}
+                  style={{ originX: "50%", originY: "50%" }}
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="absolute h-4 w-4"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 5l7 7-7 7"
+                  />
+                </motion.svg>
+              )}
+            </AnimatePresence>
+          </div>
+        )}
         {title}
       </span>
 
