09bfdedec847c6b168d48292af25f22dd0326f30
simonm <seticher.mader@gmail.com>
2022-06-06 15:58:42 +0200

    Added animation to navbar toggle

--- components/pages/_app/Navbar/Navbar.tsx
+++ components/pages/_app/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import useTranslation from "next-translate/useTranslation";
 import { useState } from "react";
 import NavbarItem from "./NavbarItem";
@@ -55,37 +55,47 @@ const Navbar = () => {
         className="bg-body bg-hero-brick-wall-purple drop-shadow-pixel-sm relative z-50 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border-2 border-black"
         onClick={() => setOpen((open) => !open)}
       >
-        {open ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        )}
+        <AnimatePresence>
+          {open ? (
+            <motion.svg
+              key="X"
+              initial={{ opacity: 0, rotate: -360 }}
+              animate={{ opacity: 1, rotate: 0 }}
+              exit={{ opacity: 0, rotate: -360 }}
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </motion.svg>
+          ) : (
+            <motion.svg
+              key="O"
+              initial={{ opacity: 0, rotate: 360 }}
+              animate={{ opacity: 1, rotate: 0 }}
+              exit={{ opacity: 0, rotate: 360 }}
+              xmlns="http://www.w3.org/2000/svg"
+              className="absolute h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </motion.svg>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
