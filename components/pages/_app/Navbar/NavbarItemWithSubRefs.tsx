21c03bb4390971bc405d3e99af3ccaf2132f0f9c
simonm <seticher.mader@gmail.com>
2022-06-06 20:36:38 +0200

    Some animation optimizations

--- components/pages/_app/Navbar/NavbarItemWithSubRefs.tsx
+++ components/pages/_app/Navbar/NavbarItemWithSubRefs.tsx
@@ -1,9 +1,9 @@
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const variants = {
   open: { opacity: 1, scaleY: 1, y: 0 },
-  closed: { opacity: 0, scaleY: 0, y: "-200%" },
+  closed: { opacity: 0, scaleY: 0, y: "-100%" },
 };
 
 const NavbarItemWithSubRefs = ({ title, subRefs }) => {
@@ -16,58 +16,33 @@ const NavbarItemWithSubRefs = ({ title, subRefs }) => {
         onClick={() => setOpen((open) => !open)}
       >
         {subRefs && (
-          <div className="relative mr-1 flex h-4 w-4 items-center justify-center">
-            <AnimatePresence initial={false}>
-              {open ? (
-                <motion.svg
-                  key="down"
-                  initial={{ opacity: 0, rotate: -90 }}
-                  animate={{ opacity: 1, rotate: 0 }}
-                  exit={{ opacity: 0, rotate: -90 }}
-                  style={{ originX: "50%", originY: "50%" }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="absolute h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19 9l-7 7-7-7"
-                  />
-                </motion.svg>
-              ) : (
-                <motion.svg
-                  key="right"
-                  initial={{ opacity: 0, rotate: 90 }}
-                  animate={{ opacity: 1, rotate: 0 }}
-                  exit={{ opacity: 0, rotate: 90 }}
-                  style={{ originX: "50%", originY: "50%" }}
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="absolute h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 5l7 7-7 7"
-                  />
-                </motion.svg>
-              )}
-            </AnimatePresence>
-          </div>
+          <motion.svg
+            animate={open ? "open" : "closed"}
+            variants={{
+              open: { rotate: 90 },
+              closed: { rotate: 0 },
+            }}
+            style={{ originX: "50%", originY: "50%" }}
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-1 h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 5l7 7-7 7"
+            />
+          </motion.svg>
         )}
         {title}
       </span>
 
       {subRefs && (
         <motion.div
-          className="bg-body bg-hero-brick-wall-purple drop-shadow-pixel-sm absolute top-16 left-0 -z-10 flex flex-col border-2 border-black"
+          className="bg-body bg-hero-brick-wall-purple absolute top-16 left-0 -z-10 flex flex-col border-2 border-black"
           animate={open ? "open" : "closed"}
           variants={variants}
           initial={false}
