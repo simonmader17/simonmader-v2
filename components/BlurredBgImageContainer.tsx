03e19aa412093371354c61dcfeee4c381b82b5c1
simonmader17 <seticher.mader@gmail.com>
2022-10-11 21:43:27 +0200

    Changed the BlurredBgImage component, so that tooltips inside it won't be cut off by clip-path

--- components/BlurredBgImageContainer.tsx
+++ components/BlurredBgImageContainer.tsx
@@ -9,7 +9,7 @@ const BlurredBgImageContainer = ({
 }) => {
   return (
     <div className="drop-shadow-pixel relative z-10 m-4" {...props}>
-      <div className="clip-rounded-pixel h-full">
+      <div className="absolute inset-0 clip-rounded-pixel h-full">
         <div className="absolute inset-0 -z-10 -m-[8px] h-[calc(100%+16px)] w-[calc(100%+16px)] blur-sm">
           <div className="relative h-full">
             <Image
@@ -22,9 +22,10 @@ const BlurredBgImageContainer = ({
             />
           </div>
         </div>
-        <div className="h-full rounded-xl bg-black bg-opacity-70 p-6">
-          {children}
-        </div>
+        <div className="absolute inset-0 z-0 bg-black bg-opacity-70"></div>
+      </div>
+      <div className="h-full p-6 relative">
+        {children}
       </div>
     </div>
   );
