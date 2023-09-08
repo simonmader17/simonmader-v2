40b1e25615c93cd5335a53fb130d53a113d330c0
simonmader17 <mail@simonmader.at>
2023-09-08 17:35:57 +0200

    Worked on linktree page

--- pages/linktree/index.tsx
+++ pages/linktree/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Container from "../../components/Container";
+import Image from "next/image";
 
 const LinktreePage = () => {
   return (
@@ -10,8 +11,32 @@ const LinktreePage = () => {
       </Head>
 
       <Container className="ltmd:mt-20">
-        <div>
-            
+        <div className="flex-shrink-0 drop-shadow-pixel">
+          <div className="clip-rounded-pixel-48 relative m-4 mx-auto h-48 w-48">
+            <Image
+              src="https://github.com/simonmader17.png"
+              alt="Simon Mader"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+        </div>
+        <h1 className="text-center">Simon Mader</h1>
+        <div className="clip-rounded-pixel my-4 flex items-center justify-start bg-black bg-opacity-70 p-4">
+          <span className="icon-gmail-white text-5xl"></span>
+          <p className="flex-grow text-center text-lg">mail@simonmader.at</p>
+          <span className="icon-gmail-white text-5xl opacity-0"></span>
+        </div>
+        <div className="clip-rounded-pixel my-4 flex items-center justify-start bg-black bg-opacity-70 p-4">
+          <span className="icon-linkedin-white text-5xl"></span>
+          <p className="flex-grow text-center text-lg">simonmader</p>
+          <span className="icon-linkedin-white text-5xl opacity-0"></span>
+        </div>
+        <div className="clip-rounded-pixel my-4 flex items-center justify-start bg-black bg-opacity-70 p-4">
+          <span className="icon-github-white text-5xl"></span>
+          <p className="flex-grow text-center text-lg">simonmader17</p>
+          <span className="icon-github-white text-5xl opacity-0"></span>
         </div>
       </Container>
     </>
