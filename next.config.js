ce598fa7286b25691eaf1dfc2a0379de46e1d4a9
simonmader17 <mail@simonmader.at>
2026-01-19 12:33:47 +0100

    Added OpenPGP public key WKD path

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = nextTranslate({
   images: {
     domains: ["github.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/openpgpkey/:path*",
+        headers: [
+          { key: "Content-Type", value: "application/pgp-keys" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ];
+  },
 });
